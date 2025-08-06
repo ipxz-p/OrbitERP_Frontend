28b1f55cbde2357f017efaf872002a228d7fad39
PongsiriSuwannasilp <64070193@it.kmitl.ac.th>
2025-08-06 22:06:12 +0700

    feat: 🎸 add toast and setup axios

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,10 +1,49 @@
-import Axios, { type InternalAxiosRequestConfig } from 'axios';
+import Axios from "axios"
+import { toast } from "sonner"
+import type {
+  AxiosError,
+  AxiosRequestConfig,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from "axios"
+import { env } from "@/config/env"
 
 function authRequestInterceptor(config: InternalAxiosRequestConfig) {
-    if (config.headers) {
-        config.headers.Accept = 'application/json';
+  config.headers.Accept = "application/json"
+  // config.withCredentials = true;
+  return config
+}
+
+export const api = Axios.create({
+  baseURL: env.API_URL,
+  // withCredentials: true,
+})
+
+api.interceptors.request.use(authRequestInterceptor)
+
+api.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response.data
+  },
+  async (error: AxiosError) => {
+    console.log(error)
+    toast.error(error.message)
+    const originalRequest = error.config as AxiosRequestConfig & { _retry?: boolean }
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true
+
+      try {
+        await api.post("/auth/refresh", {}, { withCredentials: true })
+
+        return api.request(originalRequest)
+      } catch (refreshError) {
+        window.location.href = "/login"
+        toast.error(error.message)
+        return Promise.reject(refreshError)
+      }
     }
 
-    config.withCredentials = true;
-    return config;
-}
+    return Promise.reject(error)
+  }
+)
--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,14 +1,23 @@
-import Seo from '@/utils/seo/seo'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router"
+import { useEffect } from "react"
+import { toast } from "sonner"
+import Seo from "@/utils/seo/seo"
+import { api } from "@/lib/api-client"
 
-export const Route = createFileRoute('/about')({
+export const Route = createFileRoute("/about")({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  useEffect(() => {
+    api.get("test").then((res) => {
+      toast.success("fetch success")
+      console.log(res)
+    })
+  }, [])
   return (
     <>
-      <Seo title='About' />
+      <Seo title="About" />
       <div>Hello "/about"!</div>
     </>
   )
